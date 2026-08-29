77e9a36f8630cdd34400d5da7c190bb7beb16c57
uditkanojia <uditkanojia2004@gmail.com>
2026-08-29 16:24:18 +0530

    Add files via upload

---
+++ tictactoe.js
@@ -0,0 +1,73 @@
+let boxes=document.querySelectorAll(".box");
+let resetbtn=document.querySelector("#reset-btn");
+let newgame=document.querySelector("#newgame");
+let msgcontainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
+
+let turnO=true;
+const winpatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+const resetgame=()=>{
+    let turnO=true;
+    enabledBoxes();
+    msgcontainer.classList.add("hide");
+}
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if(turnO){
+            box.innerText="O";
+            turnO=false;
+        }else{
+            box.innerText="X";
+            turnO=true;
+        }
+        box.disabled="true";
+        checkwinner();
+    });
+});
+
+const disabledBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+};
+
+const enabledBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+ 
+const showWinner=(winner)=>{
+    msg.innerText=`Congratulations,Winner is ${winner}`;
+    msgcontainer.classList.remove("hide");
+    disabledBoxes();
+}
+
+const checkwinner=()=>{
+    for(let pattern of winpatterns){
+        let pos1val=boxes[pattern[0]].innerText;
+        let pos2val=boxes[pattern[1]].innerText;
+        let pos3val=boxes[pattern[2]].innerText;
+
+        if(pos1val!="" && pos2val!="" && pos3val!=""){
+            if(pos1val===pos2val&&pos2val===pos3val){
+                showWinner(pos1val);
+            }
+        } 
+    }
+};
+
+newgame.addEventListener("click",resetgame);
+resetbtn.addEventListener("click",resetgame);
